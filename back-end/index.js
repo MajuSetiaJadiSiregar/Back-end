5eb1d70fa78b19f55bd8fe3175bc5ed3e788cbd4
majusiregar@gmail.com <47182263+bangcadel@users.noreply.github.com>
2021-02-01 16:06:38 +0700

    'connect-database'

--- back-end/index.js
+++ back-end/index.js
@@ -1,1 +1,26 @@
-console.log('test nodemon')
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+const api = process.env.API;
+const URI_DB = process.env.URI_DB;
+
+/**middleware */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(morgan('tiny'));
+
+
+
+/**connect database */
+mongoose.connect(URI_DB, { useNewUrlParser : true, useUnifiedTopology : true})
+.then(() => console.log(`database connect`))
+.catch(error => console.log(error));
+/**port */
+app.listen(`${PORT}`, () => console.log(`server run di port : ${PORT}`));
